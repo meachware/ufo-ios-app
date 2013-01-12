2c82d75c5ea9895364252822356e9c5a5003c5fc
nl00340 <sander.grout@me.com>
2013-01-12 09:40:01 +0100

    Initial Commit

---
+++ ufo/SGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SGAppDelegate.h
+//  ufo
+//
+//  Created by SandGro on 12-01-13.
+//  Copyright (c) 2013 iOnyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ufo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ufo
+//
+//  Created by SandGro on 12-01-13.
+//  Copyright (c) 2013 iOnyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([SGAppDelegate class]));
+	}
+}
